d6314e38252af2f1f651d6294a372ff248160d45
onurtaskirancom <taskiranonur9@gmail.com>
2024-01-29 09:47:10 +0300

    google analytics test2

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -18,20 +18,25 @@ export default function GoogleAnalytics({GA_MEASUREMENT_ID}: {GA_MEASUREMENT_ID:
   return (
     <>
       <Script
-        async
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
       />
       <Script
+        id="google-analytics"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', '${GA_MEASUREMENT_ID}');
-          `,
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+              
+                
+                gtag('config', '${GA_MEASUREMENT_ID}');
+                `,
         }}
       />
+  
       {/* <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
